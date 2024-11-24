78f223cc4bdadd03fb036ab02bb02610c3f4a7de
mikaylapradeepan <mikaylapradeepan@gmail.com>
2024-11-23 22:45:04 -0500

    refresh token sent to client when they login

--- pages/api/user/login/index.js
+++ pages/api/user/login/index.js
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import { comparePassword, generateToken } from "../../../../utils/auth";
+import { comparePassword, generateToken, generateRefreshToken } from "../../../../utils/auth";
 
 const prisma = new PrismaClient();
 
@@ -28,8 +28,13 @@ export default async function handler(req, res) {
             if (!token) {
                 return res.status(500).json({ message: "Couldn't generate token for user creation." });
             }
-            
-            return res.status(200).json({"access token": token});
+
+            const refreshToken = generateRefreshToken({ userId: user.id, email: user.email, role: user.role});
+            if (!refreshToken) {
+                return res.status(500).json({ message: "Couldn't generate refresh token for user creation." });
+            }
+
+            return res.status(200).json({"access token": token, "refresh token": refreshToken});
         }catch(error){
             res.status(500).json({message: "Failed login.", error: error});
         }       
