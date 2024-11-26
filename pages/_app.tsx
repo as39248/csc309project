c5a81f74c4508104f67a08f35e244377cc473863
mikaylapradeepan <mikaylapradeepan@gmail.com>
2024-11-26 15:29:36 -0500

    basic navigation bar and basic outline of home page created

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
+import React from "react";
+import { AppProps } from "next/app";  // Import AppProps type from Next.js
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import Link from "next/link";
 
+// Define your custom App component, using the correct types for Component and pageProps
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <nav className="flex flex-row bg-blue-900 text-white px-4 py-2 gap-2 flex-wrap">
+        <Link href="/">Home</Link>
+        <Link href="/IDE">IDE</Link>
+        <Link href="/Blogs">Blogs</Link>
+        <Link href="/Templates">Templates</Link>
+        <Link href="/Profile">Profile</Link>
+        <Link href="/Theme">Change Theme</Link>
+        <div className="flex-1" />
+        {/* <Link href="/logout">Logout</Link> */}
+      </nav>
+      <Component {...pageProps} /> {/* Render the page component */}
+    </>
+  );
 }
