f0a759ba3bd92e8904869155daf8fcc4073aa153
shimchan <shimchan@MSI>
2024-11-27 21:21:34 -0500

    errorfix

--- components/SearchBar/index.tsx
+++ components/SearchBar/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 interface SearchProps {
-  onSearch: (searchParams: { title?: string; content?: string; template?: string }) => void;
+  onSearch: (searchParams: { title?: string; content?: string; tag: string; template?: string }) => void;
 }
 
 const SearchBar: React.FC<SearchProps> = ({ onSearch }) => {
