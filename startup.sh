a1da595971a93224cc9affc3840a24db4afa058b
shimchan <shimchan@MSI>
2024-11-27 21:07:21 -0500

    startup

--- startup.sh
+++ startup.sh
@@ -1,12 +1,15 @@
 # Create environment variables
-export BCRYPT_SALT_ROUNDS = 10;
-export JWT_SECRET = "dfoaiefnoi2312313";
-export JWT_EXPIRES_IN = "1h";
+export BCRYPT_SALT_ROUNDS=10
+export JWT_SECRET="sfrb098j24t"
+export JWT_EXPIRES_IN="1h"
+export JWT_SECRET_REFRESH="qpegnqepg9qp"
+export JWT_EXPIRES_IN_REFRESH="7d"
 
 # install needed dependencies
 npm install
 
 # migrate prisma
+npx prisma generate
 npx prisma migrate dev --name init
 
 # TODO "this script must also create an admin user, with the username and password being included in the docs", check piazza post 305
